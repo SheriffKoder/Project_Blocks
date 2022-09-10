a4ba2811c433f56f0af5f072ee2cb92cd9c3299f
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-10 03:18:06 +0200

    Example: Rock Paper Scissors, Add Layout, Add Header and Buttons, Start working on JS part

---
+++ CP-JS-3/CPJS3.js
@@ -0,0 +1,21 @@
+
+
+let object_status = {
+
+    'start': 0 ,
+    'reset': 1,
+
+}
+
+//console.log(object_status['reset']);
+
+function start () {
+
+    if (object_status['reset']==0) {
+    object_status['start']= 1;
+
+    let rock = document.createElement('img');
+    let paper = document.createElement('img');
+    let sci = document.createElement('img');
+    }
+}
