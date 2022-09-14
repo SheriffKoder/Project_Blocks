732fcf8f68decf3247b4d6d3840f0ba818ba7e3e
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-14 21:11:36 +0200

    Add reset all buttons function

--- CP-JS-4/CPJS4.js
+++ CP-JS-4/CPJS4.js
@@ -1,5 +1,18 @@
 
 
+let all_buttons = document.getElementsByClassName('Button');
+console.log(all_buttons);
+console.log(all_buttons[0].classList[1]);
+
+let all_buttons_backup = [];
+
+for (i = 0 ; i< all_buttons.length; i++) {
+    all_buttons_backup[i] = all_buttons[i].classList[1];
+
+}
+console.log(all_buttons_backup);
+
+
 function Take_Input (x) {
 
     let input = x.value;
@@ -7,9 +20,12 @@ function Take_Input (x) {
 
     if (input == 'red') {
         console.log('Done Red');
+        set_to_red();
+
     }
     else if (input == 'reset' ) {
         console.log('Reset 2');
+        reset_buttons();
     }
     else if (input == 'random') {
         console.log('random 2');
@@ -20,11 +36,8 @@ function Take_Input (x) {
 }
 
 
-let all_buttons = document.getElementsByClassName('Button');
-console.log(all_buttons);
-console.log(all_buttons[0].classList[1]);
 
-set_to_red();
+
 
 function set_to_red () {
 
@@ -37,6 +50,34 @@ function set_to_red () {
         all_buttons[i].classList.add('RedButton');
     }
 
+    console.log ("set to red [end]");
 
+}
+
+
+function reset_buttons () {
+
+    console.log (" reset [start]");
+
+    for (i=0; i< all_buttons.length ; i++){
+
+        let currentButton = all_buttons[i].classList[1];
+        //console.log(currentButton);
+        all_buttons[i].classList.remove(currentButton);
+
+        //console.log(all_buttons_backup[i]);
+        let original_button = all_buttons_backup[i];
+        all_buttons[i].classList.add(original_button);
+
+
+
+
+
+    }
+
+
+
+
+    console.log (" reset [end]");
 
 }
