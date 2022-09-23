f8454650e42ba7e475550c7d948c329ac8992e82
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-23 20:50:08 +0200

    Add Scroll CSS property

--- w3_2209/w3_2209.js
+++ w3_2209/w3_2209.js
@@ -11,10 +11,48 @@ function myFunction () {
     var x = document.getElementById("snackbar");
 
     // Add the "show" class to DIV
-    x.className = "show";
+    x.classList.toggle('show');
+
+    setTimeout ( function () {    x.classList.toggle('show');} , 3000 );
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    //setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 
 
 }
+
+/*////////////////////////////////////////////////////////////////////*/
+
+let test22 = document.getElementById('Div_test');
+
+window.addEventListener("scroll", myFunction2);
+//window.onscroll = function() {myFunction()};
+//html <element onscroll="myScript">
+
+function myFunction2 () {
+    var scrollpercent = document.documentElement.scrollTop;
+    console.log(scrollpercent);
+
+    if (scrollpercent > 30) {
+        document.getElementById('scroll_text').style.color = "blue";
+
+    }
+    else {
+        document.getElementById('scroll_text').style.color = "red";
+    }
+
+    document.getElementById('scroll_text').innerHTML = "Scroll";
+
+}
+
+function scroll_up () {
+    //document.documentElement.scrollTop = 0;
+    document.getElementById('Div_test2').scrollTop = 0;
+
+//    element.scrollLeft += 50;
+
+    console.log("hey");
+
+}
+
+/*////////////////////////////////////////////////////////////////////*/
