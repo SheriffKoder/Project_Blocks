d4870e73449a049f91f348374ba2bb218cf8e209
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-25 22:42:14 +0200

    CSS: scrollbar show/hide, input placeholder customize, animate-like battery icon

--- w3_2209/w3_2209.js
+++ w3_2209/w3_2209.js
@@ -65,3 +65,28 @@ function scroll_up () {
 /* scrollTop either take value or give value */
 
 /*////////////////////////////////////////////////////////////////////*/
+
+/* animated icon with Font_Awesome using link , js */
+
+function chargebattery() {
+    var a;
+    a = document.getElementById("charging");
+    a.innerHTML = "&#xf244;";
+    setTimeout(function () {
+      a.innerHTML = "&#xf243;";
+    }, 1000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf242;";
+    }, 2000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf241;";
+    }, 3000);
+    setTimeout(function () {
+      a.innerHTML = "&#xf240;";
+    }, 4000);
+  }
+  chargebattery();
+  setInterval(chargebattery, 5000); /* to repeat function */
+
+
+/*////////////////////////////////////////////////////////////////////*/  
