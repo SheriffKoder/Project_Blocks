3e718a29bff3d8ad289d6c56b818624841b20895
SheriffKoder <sheriff.koder@hotmail.com>
2022-10-07 21:47:54 +0200

    CSS: border outline animations cont

--- w3_2210/w3_2210.js
+++ w3_2210/w3_2210.js
@@ -9,3 +9,23 @@ function on() {
     document.getElementById("overlay").style.display = "none";
   }
   
+
+/*////////////////////////////////////////////////////////////////////*/  
+/* on fullscreen style */
+
+  var elem = document.documentElement;
+
+  function openFullscreen() {
+    if (elem.requestFullscreen) { //exitFullscreen
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
+
+
+/*////////////////////////////////////////////////////////////////////*/
+
+
