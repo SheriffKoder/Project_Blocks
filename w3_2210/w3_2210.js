a93d2041d4142f1badb44e04c3fae5ef2a6f5aee
SheriffKoder <sheriff.koder@hotmail.com>
2022-10-17 16:32:44 +0200

    moving absolute position with js

--- w3_2210/w3_2210.js
+++ w3_2210/w3_2210.js
@@ -29,3 +29,21 @@ function on() {
 /*////////////////////////////////////////////////////////////////////*/
 
 
+
+//moving the absolute position with js
+//CSS #188 html #101 Js #33 
+/*
+var left = 0;
+
+  function frame() {
+    var element = document.querySelector('.testdiv_1710');
+    left += 2;
+    element.style.left = left + 'px';
+    if (left >= 300) {
+      clearInterval(id);
+    }
+  }
+
+  var id = setInterval(frame, 10);
+
+  frame();
