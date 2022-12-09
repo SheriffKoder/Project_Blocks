fbfae4cd8847d282b0d2bd3ad0c2852eb38a7702
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-09 14:57:42 +0200

    Notes on tabindex html, objects js

---
+++ w3_2212/w3_2212.js
@@ -0,0 +1,16 @@
+/*
+
+Object Constructor
+
+var person = new Object();  //like a function-constructor but is empty
+person.name = "diego";
+person.getName = function () {
+    return this.name;
+}
+
+
+
+
+
+
+*/
