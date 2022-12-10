51af87bcc09521b5c1ace3848ca751ea96389c57
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-10 19:38:57 +0200

    create a 3d box that has root variable can be changed to change its location and thickness also add another cube beside it

--- w3_2212/w3_2212.js
+++ w3_2212/w3_2212.js
@@ -14,3 +14,53 @@ person.getName = function () {
 
 
 */
+
+
+
+.container {
+    margin: 0px auto;
+    perspective: 900px;
+    position: relative;
+
+    width: 450px;
+
+}
+
+.frames {
+  width: 44px;
+  transform-style: preserve-3d;
+  transition:  rotateX(-10deg) rotateY(30deg) rotateZ(0deg)
+              translate3d(230px, 70px, 0px);
+}
+
+.strip {
+  transform-style: preserve-3d;
+  animation: spin 20s infinite linear;
+
+}
+
+
+.strip div {
+  position: absolute;
+  background: linear-gradient(to right, black, grey);
+  border: solid rgb(145,87,0);
+  border-width: thin 0;
+  height: 320px;
+  width: 60px;
+  opacity: 1;
+
+}
+
+.strip div:after {
+  content: "";
+  position: absolute;
+  width: 66px;
+  bottom: -40px;
+  box-shadow: 0px 20px 40px 10px rgba(0,0,0,0.3);
+  
+}
+
+.strip .s {
+  background-position: 913px;
+  transform: rotateY(15deg) translateZ(200px);
+}
